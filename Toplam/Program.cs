7885276640bf847e9699a866a1bc68060a4770c1
SAMET ARSLAN <sametarslan@stu.aydin.edu.tr>
2022-11-03 15:13:57 +0300

    Initial commit

---
+++ Toplam/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program {
+    public static void Main (string[] args) {
+        int sayi=0, sayaç=0,toplam=0;
+
+        while(sayi!=-1)
+        {
+            Console.WriteLine("Sayı girin: ");
+            sayi=Convert.ToInt32(Console.ReadLine());
+            sayaç++;
+            toplam+=sayi;
+        }
+        Console.WriteLine(toplam);
+    }
+}
