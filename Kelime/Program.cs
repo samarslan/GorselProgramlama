661b9895a2bee739bf98674c2d6aecc00ff25feb
sametarslan <sam.arslan@outlook.com>
2022-11-03 16:10:42 +0300

    Kelime büyütme programı eklendi

---
+++ Kelime/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("kelime gir: ");
+string kelime=Console.ReadLine();
+
+// foreach (char eleman in kelime)
+// {
+//     Console.WriteLine(eleman);
+// }
+
+for (int i = 0; i < kelime.Length; i++)
+{
+    Console.Write(Convert.ToChar(Convert.ToInt32(kelime[i])-32));
+}
+Console.WriteLine("\n"+kelime.ToUpper());
