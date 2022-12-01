b8639d43595a4471700d0596196aad3ac59bea6f
samarslan <samarslan@outlook.com>
2022-12-01 15:29:34 +0300

    kare projesi eklendi

---
+++ G1_Sınıflar_2/Kare.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1_Sınıflar_2
+{
+    class Kare
+    {
+        public int kenar_uzunluğu;
+
+        public int Alan_Heapla() => kenar_uzunluğu * kenar_uzunluğu;
+        public int Çevre_Heapla() => kenar_uzunluğu * 4;
+        public void Kare_Çiz()
+        {
+            for (int i = 0; i < kenar_uzunluğu; i++)
+            {
+                for (int k = 0; k < kenar_uzunluğu; k++)
+                {
+                    Console.Write('■');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ G1_Sınıflar_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1_Sınıflar_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kare nesne1 = new Kare();
+            nesne1.kenar_uzunluğu = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(nesne1.Alan_Heapla());
+            Console.WriteLine(nesne1.Çevre_Heapla());
+            nesne1.Kare_Çiz();
+            Console.ReadLine();
+        }
+    }
+}
