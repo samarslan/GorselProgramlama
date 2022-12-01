c6e4f7a1c1d194b699815095b6f28f39115324f9
samarslan <samarslan@outlook.com>
2022-12-01 15:05:22 +0300

    proje eklendi

---
+++ G1_Sınıflar-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1_Sınıflar_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(İşlem.topla(3,5));
+            İşlem sınıf1 = new İşlem();
+            sınıf1.Yazdır(İşlem.topla(3, 5));
+        }
+    }
+
+}
---
+++ G1_Sınıflar-1/İşlem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1_Sınıflar_1
+{
+    class İşlem
+    {
+        public static int topla(int a, int b)
+        {
+            return a + b;
+        }
+
+        public void Yazdır(int deger)
+        {
+            Console.WriteLine(deger);
+        }
+    }
+}
