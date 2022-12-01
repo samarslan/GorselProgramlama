62d5a72560476501a1477866e5fb91f79de85fc6
samarslan <samarslan@outlook.com>
2022-12-01 16:14:44 +0300

    okul eklendi

---
+++ G1_Sınıflar-3/Okul.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1_Sınıflar_3
+{
+    class Okul
+    {
+        public int sınıf_adet;
+        public string okul_isim;
+        public int öğrenci_sayısı;
+
+        public Okul(int a, string b, int c)
+        {
+            sınıf_adet = a;
+            okul_isim = b;
+            öğrenci_sayısı = c;
+        }
+        public void Yazdır()
+        {
+            Console.WriteLine("okulumuzdae {0} adet sınıf vardır. {1} okulumuzun ismidir. {2} Öğrenci sayısı = {2}", sınıf_adet, okul_isim, öğrenci_sayısı);
+        }
+    }
+}
---
+++ G1_Sınıflar-3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1_Sınıflar_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Okul okul = new Okul(20, "Salamcılar Anadolu Lisesi", 20);
+            okul.Yazdır();
+
+            Okul okul2 = new Okul(30, "Atatürk Anadolu Lisesi", 9000);
+            okul2.Yazdır();
+        }
+    }
+}
